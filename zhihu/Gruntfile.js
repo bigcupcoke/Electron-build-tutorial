b6e042902d1bba1e19d6f2696db67afd4ee788d0
coke.dj <dujun.sc@outlook.com>
2017-08-16 15:10:10 +0800

    version 1.1

--- zhihu/Gruntfile.js
+++ zhihu/Gruntfile.js
@@ -4,13 +4,15 @@ grunt.config.init({
     pkg: grunt.file.readJSON('package.json'),
     'create-windows-installer': {
         x64: {
+            version: '1.1.0',
             appDirectory: './zhihu-win32-x64',
-            authors: 'yohnz.',            
-            iconUrl:'http://7xn0vc.com1.z0.glb.clouddn.com/zhihu_logo.ico',                    
-            exe: 'zhihu.exe',  
+            authors: 'yohnz.',
+            iconUrl:'http://7xn0vc.com1.z0.glb.clouddn.com/zhihu_logo.ico',
+            exe: 'zhihu.exe',
             description:"zhihu",
             setupIcon:"logo.ico",
-            noMsi:true            
+            noMsi:false,
+            remoteReleases: 'https://github.com/bigcupcoke/Electron-build-tutorial'
         }
     }
 })
